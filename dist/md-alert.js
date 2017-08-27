dd91e7f716e3f80f11d8dd8d57cd3f566fd06b54
Dipanjan Kundu <dipanjan@novatree.com>
2017-08-27 20:26:24 +0530

    js issue for multiple alert opening fixed

--- dist/md-alert.js
+++ dist/md-alert.js
@@ -2,11 +2,13 @@ let mdalert = function () {
 
     let displayAlert_ = function (title="Sorry", text="Error") {
 
+        $(".md-modal").remove();
+
         let alertDiv = "<div id=\"md-modal\" class=\"md-modal\">\n" +
             "    <div class=\"modal-content enter-alert\">\n" +
-            "        <h3 class=\"modal-title\"><h3>\n" +
+            "        <h3 class=\"modal-title\"></h3>\n" +
             "        <h6 class=\"modal-text\"></h6>\n" +
-            "        <button class=\"modal-button\" id='modal-button' autofocus >OK</button>\n" +
+            "        <button class=\"modal-button\" id='modal-button' autofocus='autofocus' >OK</button>\n" +
             "    </div>\n" +
             "</div>";
 
@@ -17,7 +19,6 @@ let mdalert = function () {
         $(".modal-title").text(title);
         $(".modal-text").text(text);
 
-        let modal = document.getElementById("md-modal");
         let ok = document.getElementById("modal-button");
 
         $(window).click(function (event) {
