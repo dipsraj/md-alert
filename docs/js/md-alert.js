89557229aefea5b3eec76ca8fdb56062bcc20562
Dipanjan Kundu <dipanjan@novatree.com>
2017-08-28 01:26:36 +0530

    Folders re-structured

---
+++ docs/js/md-alert.js
@@ -0,0 +1,39 @@
+let mdalert = function () {
+
+    let displayAlert_ = function (title="Sorry", text="Error") {
+
+        $(".md-modal").remove();
+
+        let alertDiv = "<div id=\"md-modal\" class=\"md-modal\">\n" +
+            "    <div class=\"modal-content enter-alert\">\n" +
+            "        <h3 class=\"modal-title\"></h3>\n" +
+            "        <h6 class=\"modal-text\"></h6>\n" +
+            "        <button class=\"modal-button\" id='modal-button' autofocus='autofocus' >OK</button>\n" +
+            "    </div>\n" +
+            "</div>";
+
+        $("body").append(alertDiv);
+
+        // alertDiv.appendTo($("body")).slideDown("fast");
+
+        $(".modal-title").text(title);
+        $(".modal-text").text(text);
+
+        let ok = document.getElementById("modal-button");
+
+        $(window).click(function (event) {
+            if (event.target === ok) {
+                $(".modal-content").addClass("exit-alert");
+                $(".md-modal").fadeOut(312, function() { $(this).remove(); });
+            }
+        });
+
+    };
+
+    return{
+        alert:function (title, text) {
+            displayAlert_(title, text);
+        }
+    }
+
+}();
