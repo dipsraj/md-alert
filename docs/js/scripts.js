a5fed16eb4655d9cee77316d99e0b0b7a229189a
Dipanjan Kundu <dipanjan@novatree.com>
2017-08-28 00:23:39 +0530

    Alert CSS fixed issue regarding height

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,6 +1,10 @@
 $("#button").click(function (e) {
     e.preventDefault();
     let title = $("#title").val();
+    if(title.length<1)
+        title="Warning";
     let text = $("#text").val();
+    if(text.length<1)
+        text="No Text Entered";
     mdalert.alert(title,text);
 });
